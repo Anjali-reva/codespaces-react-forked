b896b3abe591be0a86c5cfa02c0755b60645dbd0
Sakib Zalori <142202725+Denny9113@users.noreply.github.com>
2023-12-30 10:38:39 +0000

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Icon } from '@iconify/react';
+
+function Header() {
+  return (
+    <div>
+        <p className='underline'>sakib zalori</p>
+        <Icon icon="mdi:menu" />
+    </div>
+  )
+}
+
+export default Header
